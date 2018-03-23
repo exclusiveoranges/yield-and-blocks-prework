c6251acacc2c6332040939daac306b2de58c3156
Ryan O'Toole <otoolerj@gmail.com>
2018-03-23 14:42:43 +0000

    automatically backed up by learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,29 +1,22 @@
-# def hello_t(array)
-#   if block_given?
-#     i = 0
-#   while i < array.length
-#    yield (array[i])
-#    i += 1
-#  end
-#  else
-#    puts "Hey! No block was given!"
-#  end
-#  array
-# end
-
-
-
 def hello_t(array)
+  if block_given?
     i = 0
   while i < array.length
    yield (array[i])
    i += 1
  end
+ else
+   puts "Hey! No block was given!"
+ end
  array
 end
 
-hello_t(["Tim", "Tom", "Jim"]) do |name|
-  if name.start_with?("T")
-    puts "Hi, #{name}"
-  end
-end
+
+
+
+# 
+# hello_t(["Tim", "Tom", "Jim"]) do |name|
+#   if name.start_with?("T")
+#     puts "Hi, #{name}"
+#   end
+# end
