8e5026c7e8e6eac6667b3e3ad5b7fd8967c6849c
Ryan O'Toole <otoolerj@gmail.com>
2018-03-23 14:40:42 +0000

    automatically backed up by learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,15 +1,15 @@
-def hello_t(array)
-  if block_given?
-    i = 0
-  while i < array.length
-   yield (array[i])
-   i += 1
- end
- else
-   puts "Hey! No block was given!"
- end
- array
-end
+# def hello_t(array)
+#   if block_given?
+#     i = 0
+#   while i < array.length
+#    yield (array[i])
+#    i += 1
+#  end
+#  else
+#    puts "Hey! No block was given!"
+#  end
+#  array
+# end
 
 # hello_t(["Tim", "Tom", "Jim"]) do |name|
 #   if name.start_with?("T")
@@ -25,5 +25,3 @@ def hello_t(array)
  end
  array
 end
-
-
